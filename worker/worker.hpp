3e8948802ac4af0a988e0c070d1528d94f07227a
ydwu4 <ydwu2014@gmail.com>
2018-04-16 21:18:10 +0800

    [Worker] finish basic implementation. Need to be tested

--- worker/worker.hpp
+++ worker/worker.hpp
@@ -6,6 +6,8 @@ extern"C"{
 
 #include <thread>
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <vector>
 #include <string>
 #include <utility>
@@ -45,19 +47,20 @@ using namespace std;
 			if (res != 0) {
 				cout << "worker: connect to " << master_point << " fails with error:" << strerror(errno) << endl;				
 			}
-			fprintf(stderr, "initialize socket succeeds\n");
+
 		}
 		
 		// this is the main function
 		void serve() {
 			fprintf(stderr, "start to serve\n");
-
 			int rc = 0;
 			zmq_msg_t msg;
 			rc = zmq_msg_init(&msg);
 			if (rc != 0) {
 				cout << "worker fails to init zmq msg" << endl;
 			}
+
+
 
 			this->running = true;
 			while(this->running) {
@@ -79,7 +82,6 @@ using namespace std;
 				char *data = (char*) zmq_msg_data(&msg);
 				assert (data != NULL);
 				
-				cout << "msg type:" << data[0] << endl;
 				switch(data[0]) {
 				case '1':
 					init(&msg);
@@ -94,10 +96,9 @@ using namespace std;
 				case '6':
 					get_metadata(&msg);
 				default:
-					cout << "error unknown message type with:" << data[0] << endl;
+					cout << "error unknown message type" << endl;
 				}	
 			}
-
 			cout << "worker exit main loop and try to close msg" << endl;
 			rc = zmq_msg_close(&msg);	
 			if (rc != 0) {
@@ -123,20 +124,134 @@ using namespace std;
 			}
 		}
 
+		// 2, 
+		// filename 
+		// offset len 
 		void small_file_read(zmq_msg_t *msg) {
+			vector<string> vec;
+			recv_more(vec, msg);
 
+			std::string filename = vec[1];
+			vector<string> meta_vec = parse_to_vector(vec[2]);
+			int offset = stoi(meta_vec[0]);
+			int len = stoi(meta_vec[1]) * len;
+
+			char* buffer = (char*)malloc(sizeof(char) *len);
+			local_read(filename, len, offset, buffer);
+			send_to_master(buffer, len);
+			free(buffer);
 		}
+
+		// 4, 
+		// filename 
+		// len offset 
+		// data
+		void small_file_write(zmq_msg_t *msg) {
+			vector<string> vec;
+			recv_more(vec, msg);
+
+			std::string filename = vec[1];
+
+			vector<string> meta_vec = parse_to_vector(vec[2]);
+			int len = stoi(meta_vec[0]);
+			int offset = stoi(meta_vec[1]) * len;
+			const char* buf = vec[3].c_str();
+
+			local_write(filename, len, offset, buf);
+
+			int ok_len = 2;
+			char * ok_msg = (char*)malloc(sizeof(char) * ok_len);
+			strncpy(ok_msg, "ok", ok_len);
+			send_to_master(ok_msg, ok_len);
+			free(ok_msg);
+		}
+
+		// 3, filename len offset
+		// [filename len offset]*
 
 		void large_file_read(zmq_msg_t *msg) {
+			int filename_idx = 0;
+			int len_idx = 1;
+			int offset_idx = 2;
+          	vector<string> vec;
+			recv_more(vec, msg);
 
+
+			// parse meta
+			vector<vector<string>> meta_vec;
+			for (int i=1; i<vec.size(); i++) {
+				meta_vec.push_back(parse_to_vector(vec[i]));
+			}
+
+			// compute total_len
+			int total_len = stoi(meta_vec[0][len_idx]);
+			for (int i=1; i<meta_vec.size(); i++) {
+				assert(stoi(meta_vec[i-1][len_idx]) + stoi(meta_vec[i-1][offset_idx]) == stoi(meta_vec[i][offset_idx]));
+				total_len += stoi(meta_vec[i][len_idx]);
+			}
+
+			int initial_offset = stoi(meta_vec[0][offset_idx]);
+			string filename = meta_vec[0][filename_idx];
+
+			char* buffer = (char*)malloc(sizeof(char) * total_len);
+			local_read(filename, total_len, initial_offset, buffer);
+			send_to_master(buffer, total_len);
+			free(buffer);
 		}
 
-		void small_file_write(zmq_msg_t *msg) {
-
-		}
-
+		// assume message is the form of
+		// 6, 
+		// filename len1 offset1,
+		// data1
+		// [string:filename int:len2 int:offset2 string:data2]
+		// [....]
 		void large_file_write(zmq_msg_t *msg) {
+			int name_idx = 0;
+			int len_idx = 1;
+			int offset_idx = 2;
+			vector<string> vec;
+			recv_more(vec, msg);
+			// assert for the continuity of data blocks
 
+			// parse meta data
+			vector<vector<string>> meta_vec;
+			for(int i = 1; i<vec.size(); i+=2) {
+				meta_vec.push_back(parse_to_vector(vec[i]));
+			}
+			// get data string ptr
+			vector<const char*> buf_ptr;
+			for (int i=2; i<vec.size(); i+=2) {
+				buf_ptr.push_back(vec[i].c_str());
+			}
+			assert(buf_ptr.size() == meta_vec.size());
+
+			// get total len
+			int total_len = stoi(meta_vec[0][len_idx]);
+			for (int i=1; i<meta_vec.size(); i++) {
+				assert(stoi(meta_vec[i-1][len_idx]) + stoi(meta_vec[i-1][offset_idx]) == stoi(meta_vec[i][offset_idx]));
+				total_len += stoi(meta_vec[i][len_idx]);
+			}
+
+			char* data = (char*) malloc(sizeof(char) * total_len);
+			// copy to new chunk
+			int offset = 0;
+			for (int i=0; i<buf_ptr.size(); i++) {
+				int len = stoi(meta_vec[i][len_idx]);
+				memmove(data+offset, buf_ptr[i], len);
+				offset += len;
+			}
+			assert(offset == total_len);
+
+			string filename = meta_vec[0][name_idx];
+			int initial_offset = stoi(meta_vec[0][offset_idx]); 
+			local_write(filename, total_len, initial_offset, data);
+
+
+			int ok_len = 2;
+			char * ok_msg = (char*)malloc(sizeof(char) * ok_len);
+			strncpy(ok_msg, "ok", ok_len);
+			send_to_master(ok_msg, ok_len);
+			free(data);
 		}
 
 		void get_metadata(zmq_msg_t *msg) {
@@ -161,4 +276,66 @@ using namespace std;
 
 		void *context;
 		void *socket;
+	private:	
+		vector<string> parse_to_vector(string str) {
+			istringstream iss(str);
+			vector<string> vec;
+			string tmp;
+			while (getline( iss, tmp, ' ' )) {
+			   	vec.push_back(tmp);
+			}
+			cout<<"parsed: ";
+			for(auto& a : vec) {
+			    cout<< a <<",";
+			}
+			cout<<endl;
+
+			return vec;			
+		}
+		void local_read(string filename, int len, int offset, char* buf) {
+			ifstream in(filename, ifstream::in | ifstream::binary);
+			if (in.is_open()) {
+				in.seekg(offset);
+				in.read(buf, len);
+				in.close();				
+			} else {
+				cout<< "Enable to open file " <<filename << endl;
+			}
+
+		}
+		void local_write(string filename, int len, int offset, const char* buf) {
+			ofstream out(filename, ifstream::out | ifstream::binary);
+			if (out.is_open()) {
+				out.seekp(offset);
+				out.write(buf, len);
+				out.close();
+			} else {
+				cout<< "Enable to open file " << filename <<endl;
+			}
+		}
+
+		void send_to_master(char* data, int len) {
+			zmq_msg_t msg;
+			zmq_msg_init_data(&msg, data, len, NULL, NULL);
+			int sent_size = zmq_msg_send(&msg, this->socket, 0);
+			assert(sent_size == len);
+		}
+
+		void recv_more(vector<string>& buf, zmq_msg_t* msg) {
+			buf.clear();
+
+			char *data = (char*) zmq_msg_data(msg);
+			string part(data);
+			buf.push_back(part);
+			while(zmq_msg_more(msg)) {
+				int size = zmq_msg_recv(msg, this->socket, 0);
+				if (size < 0) {
+					cout << "recv_more: worker fails to recieve message about operations" << endl;
+				}
+				data = (char*) zmq_msg_data(msg);
+				assert (data != NULL);
+				string p(data);
+				buf.push_back(p);
+			}
+		}
 	};
